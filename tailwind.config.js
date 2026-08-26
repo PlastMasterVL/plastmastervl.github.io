a8bda4e3dc54175228f7bdb0fdd05e9e84452db7
kamigawasatoru-blip <kamigawasatoru@gmail.com>
2026-08-26 13:09:45 +0900

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
@@ -10,6 +11,7 @@ export default {
         sage: { DEFAULT: "#8B9A8C", dark: "#5F7361" },
         stone: { DEFAULT: "#8A8478", dark: "#5C574C" },
         line: "#E8E1D4",
+        cart: { DEFAULT: "#CB11AB", dark: "#A20E8A" },
       },
     },
   },
